167cd77cf1e2fbe0efae8c0272407bee5cfa35d2
connie <conniedixon106@gmail.com>
2020-03-17 16:56:30 +0000

    Added plant page functionality

--- sprout/components/PlantPage.tsx
+++ sprout/components/PlantPage.tsx
@@ -1,5 +1,8 @@
 /** @format */
 
+interface Props {
+  navigation: any;
+}
 import React from 'react';
 import {
   StyleSheet,
@@ -14,16 +17,20 @@ const PlantPage = ({ route }) => {
   const { plantInfo, plantImage } = route.params;
   return (
     <View>
+      <Image
+        style={{ width: 350, height: 300 }}
+        source={{ uri: `data:image/gif;base64,${plantImage}` }}
+      />
       <Text>
-        {plantInfo.commonName}, {plantInfo.scientificName}
-        Duration: {plantInfo.duration}, Family: {plantInfo.family}, Difficulty:{' '}
+        {plantInfo.commonName}, {plantInfo.scientificName},Duration:{' '}
+        {plantInfo.duration}, Family: {plantInfo.family}, Difficulty:{' '}
         {plantInfo.difficulty}, Care Instructions: Light level:{' '}
         {plantInfo.lightLevel}, Soil pH: {plantInfo.ph}, Watering Needs:{' '}
         {plantInfo.wateringSchedule}
       </Text>
-      <Image
-        style={{ width: 350, height: 300 }}
-        source={{ uri: `data:image/gif;base64,${plantImage}` }}
+      <Button
+        title='Add to My Garden'
+        onPress={() => console.log('This does nothing')} // Not functional
       />
     </View>
   );

d2f60cbbf58de55edab5483150f15699122dfd40
connie <conniedixon106@gmail.com>
2020-03-17 19:47:08 +0000

    Edited api to include functions from both front end pushes

--- sprout/api.ts
+++ sprout/api.ts
@@ -1,18 +1,18 @@
 /** @format */
-
-const axios = require("axios");
+import * as utils from './utils/utils';
+const axios = require('axios');
 
 export const getPlantById = (base64: any) => {
-  console.log("in the api");
+  console.log('in the api');
   const plantImg = { images: [base64] };
   let axiosConfig = {
     headers: {
-      "Content-Type": "application/json",
-      "Api-Key": "oEk029eOZyIto6lZoAWI4WKqVxPiV4Tt6Dl3JMIGDOXQ3V3Uu9"
+      'Content-Type': 'application/json',
+      'Api-Key': 'oEk029eOZyIto6lZoAWI4WKqVxPiV4Tt6Dl3JMIGDOXQ3V3Uu9'
     }
   };
   return axios
-    .post("https://api.plant.id/v2/identify", plantImg, axiosConfig)
+    .post('https://api.plant.id/v2/identify', plantImg, axiosConfig)
     .then(({ data: { suggestions } }) => {
       const scientificName = suggestions[0].plant_details.scientific_name;
 
@@ -24,7 +24,7 @@ export const getPlantById = (base64: any) => {
 };
 
 function getPlantByName(scientificName) {
-  console.log("in the second function");
+  console.log('in the second function');
   return axios
     .get(
       `https://trefle.io/api/plants?token=aXVMMTJIOTBXaHI2STlibXFOTGZndz09&&scientific_name=${scientificName}`
@@ -39,7 +39,7 @@ function getPlantByName(scientificName) {
 }
 
 function getSingularPlant(plantId) {
-  console.log("in the third function");
+  console.log('in the third function');
   return axios
     .get(
       `https://trefle.io/api/plants/${plantId}?token=aXVMMTJIOTBXaHI2STlibXFOTGZndz09`
@@ -47,8 +47,8 @@ function getSingularPlant(plantId) {
     .then(({ data }) => {
       const plantFamilyId = data.main_species.sources[0].species_id;
       const plantData = {
-        common_name: data.main_species.common_name,
-        scientific_name: data.scientific_name
+        commonName: data.main_species.common_name,
+        scientificName: data.scientific_name
       };
       return getCareInstructions(plantFamilyId, plantData);
     })
@@ -58,32 +58,32 @@ function getSingularPlant(plantId) {
 }
 
 const getCareInstructions = (plantFamilyId, plantData) => {
-  console.log("in the fourth function");
+  console.log('in the fourth function');
   return axios
     .get(
       `https://trefle.io/api/species/${plantFamilyId}?token=aXVMMTJIOTBXaHI2STlibXFOTGZndz09`
     )
     .then(({ data }) => {
       const careInstructions = {
-        min_temp: data.growth.temperature_minimum.deg_c,
-        shade_tolerance: data.growth.shade_tolerance,
-        precipitation_min: data.growth.precipitation_minimum.cm,
-        precipitation_max: data.growth.precipitation_maximum.cm,
-        ph_minimum: data.growth.ph_minimum,
-        ph_maximum: data.growth.ph_maximum,
-        fertility_requirement: data.growth.fertility_requirement,
-        drought_tolerance: data.growth.drought_tolerance,
         family: data.family_common_name,
-        duration: data.duration
+        duration: data.duration,
+        droughtTol: data.growth.drought_tolerance,
+        shadeTol: data.growth.shade_tolerance,
+        fertility: data.growth.fertility_requirement,
+        minTemp: data.growth.temperature_minimum.deg_c,
+        phMax: data.growth.ph_maximum,
+        phMin: data.growth.ph_minimum,
+        waterMax: data.growth.precipitation_maximum.cm,
+        waterMin: data.growth.precipitation_minimum.cm
       };
-      const plantInfo = { ...careInstructions, ...plantData };
-      return plantInfo;
+      const plantInfo = { ...plantData, ...careInstructions };
+      return utils.getStats(plantInfo);
     })
     .catch(err => console.log(err));
 };
 
 export const getScientificName = searchText => {
-  console.log("getting the scientific_name by querying the common name");
+  console.log('getting the scientific_name by querying the common name');
   return axios
     .get(
       `https://trefle.io/api/plants?common_name=${searchText}&&token=aXVMMTJIOTBXaHI2STlibXFOTGZndz09`

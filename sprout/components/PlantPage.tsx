3e334d713b6dc10da8758d007cf89188aac2efa6
connie <conniedixon106@gmail.com>
2020-03-17 16:28:54 +0000

    Added functionality to plant response, edited names on util functions to correspond with api

--- sprout/components/PlantPage.tsx
+++ sprout/components/PlantPage.tsx
@@ -12,17 +12,14 @@ import {
 
 const PlantPage = ({ route }) => {
   const { plantInfo, plantImage } = route.params;
-  console.log(plantInfo, '<--- plant Info here');
   return (
     <View>
       <Text>
         {plantInfo.commonName}, {plantInfo.scientificName}
-        Duration: {plantInfo.duration}
-        Family: {plantInfo.family}
-        Difficulty: {plantInfo.difficulty}
-        Care Instructions: Light level: {plantInfo.lightLevel}
-        Soil pH: {plantInfo.ph}
-        Watering Needs: {plantInfo.wateringSchedule}
+        Duration: {plantInfo.duration}, Family: {plantInfo.family}, Difficulty:{' '}
+        {plantInfo.difficulty}, Care Instructions: Light level:{' '}
+        {plantInfo.lightLevel}, Soil pH: {plantInfo.ph}, Watering Needs:{' '}
+        {plantInfo.wateringSchedule}
       </Text>
       <Image
         style={{ width: 350, height: 300 }}

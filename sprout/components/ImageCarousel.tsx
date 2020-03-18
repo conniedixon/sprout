ca530927a3bb07bad8ebbee0350df94219468d5b
anisapatel <anisa_patel@live.co.uk>
2020-03-18 14:18:42 +0000

    currently working on an image carousel component

--- sprout/components/ImageCarousel.tsx
+++ sprout/components/ImageCarousel.tsx
@@ -1,9 +1,55 @@
 import React, { Component } from "react";
+import {
+  View,
+  ScrollView,
+  Image,
+  StyleSheet,
+  Dimensions,
+  ImageStore
+} from "react-native";
 
-class ImageCarousel extends Component {
+// const width = 400;
+// const height = 400;
+
+interface ImagesProps {
+  images: any;
+}
+
+class ImageCarousel extends Component<ImagesProps> {
   render() {
-    return <div></div>;
+    // console.log(this.props.images.images.flat(), "<---images");
+    const images = this.props.images.images.flat();
+    if (this.props.images && this.props.images.length) {
+      return (
+        <View>
+          <ScrollView
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+          >
+            {images.map(image =>
+              console.log(image)
+              //   <Image
+              //     // style={{ width: 350, height: 300 }}
+              //     source={{ uri: image.url }}
+              //   />
+            )}
+          </ScrollView>
+        </View>
+      );
+    }
+    return null;
   }
 }
 
 export default ImageCarousel;
+
+// const styles = StyleSheet.create({
+//   scrollContainer: {
+//     height
+//   },
+//   image: {
+//     width,
+//     height
+//   }
+// });

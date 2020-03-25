d10a1fcc4c6939950ade7c21247c7e79fdd371fd
Imo-Hetherington <hetherington.ir@gmail.com>
2020-03-25 11:00:33 +0000

    added plantInfo and uri as seperate keys when getting images from s3 to avoid posting base64 image into garden.

--- sprout/components/ScannedPlants.tsx
+++ sprout/components/ScannedPlants.tsx
@@ -38,10 +38,10 @@ class ScannedPlants extends Component<Props> {
               <TouchableOpacity
                 onPress={() => {
                   this.props.navigation.navigate("PlantPage", {
-                    plantInfo: plant,
+                    plantInfo: plant.plantInfo,
                     isInGarden: false,
                     username: username,
-                    plantImage: plant.uri, //needs to be changed
+                    plantImage: plant.uri,
                   });
                 }}
                 key={plant.timestamp}

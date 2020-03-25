eb616143090105d9ea298df634e54531a0e7bb00
connie <conniedixon106@gmail.com>
2020-03-25 08:44:48 +0000

    Styled plant page

--- sprout/components/PlantPage.tsx
+++ sprout/components/PlantPage.tsx
@@ -1,12 +1,13 @@
 /** @format */
 
 import React, { Component } from "react";
-import { Text, View, Button, Image, Alert } from "react-native";
+import { Text, View, Alert, StyleSheet, ImageBackground } from "react-native";
 import ImageCarousel from "./ImageCarousel";
 import { addPlantToGarden, addToWishlist } from "./spec/index";
 import { getUserScannedPlants } from "../components/spec/index";
 
 import { addNotifications } from "./spec/notifications";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const PlantPage = ({ route, navigation }) => {
   const { plantInfo, plantImage, isInGarden, username } = route.params;
@@ -73,80 +74,166 @@ const PlantPage = ({ route, navigation }) => {
   if (isInGarden === "isInWishlist") {
     return (
       <View>
-        {MedalsAlert()}
-        <ImageCarousel images={images} />
-        <Text>
-          {plantInfo.commonName}, {plantInfo.scientificName},Duration:{" "}
-          {plantInfo.duration}, Family: {plantInfo.family}, Difficulty:{" "}
-          {plantInfo.difficulty}, Care Instructions: Light level:{" "}
-          {plantInfo.lightLevel}, Soil pH: {plantInfo.ph}, Watering Needs:{" "}
-          {plantInfo.wateringSchedule}
-        </Text>
-        <Button
-          title="Add to My Garden"
-          onPress={() =>
-            addPlantToGarden(plantInfo, username).then(() => {
-              AsyncAlert("your Garden", "MyGarden");
-            })
-          }
-        />
+        <ImageBackground
+          source={require("./graphics/Background.jpg")}
+          style={styles.backgroundImage}
+        >
+          <View style={styles.container}>
+            <ImageCarousel images={images} />
+
+            <Text style={styles.header}>{plantInfo.commonName}</Text>
+            <Text style={styles.subheader}>{plantInfo.scientificName}</Text>
+            <Text style={styles.text}>Duration: {plantInfo.duration}</Text>
+            <Text style={styles.text}> Family: {plantInfo.family}</Text>
+            <Text style={styles.text}>Difficulty: {plantInfo.difficulty}</Text>
+            <Text style={styles.subheader}>How to care for me:</Text>
+            <Text>Light level:{plantInfo.lightLevel} </Text>
+            <Text>Soil pH: {plantInfo.ph}</Text>
+
+            <Text>Watering Needs:{plantInfo.wateringSchedule} </Text>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() =>
+                addPlantToGarden(plantInfo, username).then(() => {
+                  AsyncAlert("your Garden", "MyGarden");
+                })
+              }
+            >
+              <Text style={styles.button}>Add to My Garden</Text>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
       </View>
     );
   } else if (!isInGarden) {
     return (
       <View>
-        <ImageCarousel images={images} />
+        <ImageBackground
+          source={require("./graphics/Background.jpg")}
+          style={styles.backgroundImage}
+        >
+          <View style={styles.container}>
+            <ImageCarousel images={images} />
 
-        <Text>
-          {plantInfo.commonName}, {plantInfo.scientificName},Duration:{" "}
-          {plantInfo.duration}, Family: {plantInfo.family}, Difficulty:{" "}
-          {plantInfo.difficulty}, Care Instructions: Light level:{" "}
-          {plantInfo.lightLevel}, Soil pH: {plantInfo.ph}, Watering Needs:{" "}
-          {plantInfo.wateringSchedule}
-        </Text>
-        <Button
-          title="Add to My Garden"
-          onPress={() =>
-            addPlantToGarden(plantInfo, username).then(() => {
-              AsyncAlert("your Garden", "MyGarden");
-            })
-          }
-        />
-        <Button
-          title="Add to My Wishlist"
-          onPress={() =>
-            addToWishlist(plantInfo, username).then(() => {
-              AsyncAlert("your Wishlist", "Wishlist");
-            })
-          }
-        />
-        <Button
-          title="Scan Another Plant"
-          onPress={() => navigation.navigate("CameraPage")}
-        />
+            <Text style={styles.header}>{plantInfo.commonName}</Text>
+            <Text style={styles.subheader}>{plantInfo.scientificName}</Text>
+            <Text style={styles.text}>Duration: {plantInfo.duration}</Text>
+            <Text style={styles.text}> Family: {plantInfo.family}</Text>
+            <Text style={styles.text}>Difficulty: {plantInfo.difficulty}</Text>
+            <Text style={styles.subheader}>How to care for me:</Text>
+            <Text>Light level:{plantInfo.lightLevel} </Text>
+            <Text>Soil pH: {plantInfo.ph}</Text>
+
+            <Text>Watering Needs:{plantInfo.wateringSchedule} </Text>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() =>
+                addPlantToGarden(plantInfo, username).then(() => {
+                  AsyncAlert("your Garden", "MyGarden");
+                })
+              }
+            >
+              <Text style={styles.button}>Add to My Garden</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() =>
+                addToWishlist(plantInfo, username).then(() => {
+                  AsyncAlert("your Wishlist", "Wishlist");
+                })
+              }
+            >
+              <Text style={styles.button}>Add to My Wishlist</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => navigation.navigate("CameraPage")}
+            >
+              <Text style={styles.button}>Scan Another Plant</Text>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
       </View>
     );
   } else {
     return (
       <View>
-        <ImageCarousel images={images} />
+        <ImageBackground
+          source={require("./graphics/Background.jpg")}
+          style={styles.backgroundImage}
+        >
+          <View style={styles.container}>
+            <ImageCarousel images={images} />
+            <View>
+              <Text style={styles.header}>{plantInfo.commonName}</Text>
+              <Text style={styles.subheader}>{plantInfo.scientificName}</Text>
+              <Text style={styles.text}>Duration: {plantInfo.duration}</Text>
+              <Text style={styles.text}> Family: {plantInfo.family}</Text>
+              <Text style={styles.text}>
+                Difficulty: {plantInfo.difficulty}
+              </Text>
+              <Text style={styles.subheader}>How to care for me:</Text>
+              <Text style={styles.text}>
+                Light level:{plantInfo.lightLevel}{" "}
+              </Text>
+              <Text style={styles.text}>Soil pH: {plantInfo.ph}</Text>
+              <Text style={styles.text}>
+                Watering Needs:{plantInfo.wateringSchedule}{" "}
+              </Text>
+            </View>
 
-        <Text>
-          {plantInfo.commonName}, {plantInfo.scientificName},Duration:{" "}
-          {plantInfo.duration}, Family: {plantInfo.family}, Difficulty:{" "}
-          {plantInfo.difficulty}, Care Instructions: Light level:{" "}
-          {plantInfo.lightLevel}, Soil pH: {plantInfo.ph}, Watering Needs:{" "}
-          {plantInfo.wateringSchedule}
-        </Text>
-        <Button
-          title="Set Reminder"
-          onPress={() => {
-            addNotifications(plantInfo);
-          }}
-        />
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                addNotifications(plantInfo);
+              }}
+            >
+              <Text style={styles.button}>Remind Me To Water</Text>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
       </View>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+  },
+  button: {
+    backgroundColor: "#aebb8f",
+    color: "white",
+    fontSize: 15,
+    textAlign: "center",
+    alignSelf: "center",
+    width: "50%",
+    justifyContent: "space-around",
+    margin: 3,
+  },
+  header: {
+    fontSize: 28,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 14,
+  },
+  subheader: {
+    fontSize: 20,
+    textAlign: "center",
+    fontStyle: "italic",
+  },
+  container: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 5,
+  },
+  imagecontainer: {},
+});
 
 export default PlantPage;
